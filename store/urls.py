23feb185012f7d416c17bdc346daf4154582f7d9
Your Name <you@example.com>
2025-12-02 14:03:36 +0300

    feat(posts): Добавил функционал получения деталей поста: - шаблон с деталями поста - маршрут и обработчик для этого маршрута

--- store/urls.py
+++ store/urls.py
@@ -4,8 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # path("", ) # Временно закомментировали маршрут на главную страницу. С ней будем работать позже.
-  path("products/", views.get_product_list) # Вместо "products" можно указать, что угодно. 
-  #"/" после него ставить обязательно. + "views.get_product_list" пишем без круглых скобок, 
-  # потому что если написать так: "views.get_product_list()", то это будет вызов функции. Здесь это не нужно.
+    # path("", ) # Временно закомментировали маршрут на главную страницу. С ней будем работать позже.
+    path("products/", views.get_product_list, name='product_list'),
+    path("products/<int:product_id>/", views.get_post_detail, name='product_detail'),
 ]
