417ff275badbf45b2c2b3c38503c5811d9b5592d
Your Name <you@example.com>
2025-12-02 14:06:42 +0300

    feat(posts): Добавил функционал получения деталей поста: - шаблон с деталями поста - маршрут и обработчик для этого маршрута

--- store/views.py
+++ store/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from store.models import Products
 
-def get_product_list(request): # "request" в скобках указывать обязательно, можно поставить другое слово, 
-  # но лучше "request".
+# "request" в скобках указывать обязательно, можно поставить другое слово, 
+# но лучше "request".
+def get_product_list(request): 
   products = Products.objects.all()
 
-  return render(request, 'store/index.html', context={'products': products}) # Здесь уже нужно создать 
-      # первый шаблон. Путь 'blog/index.html' здесь не нужно указывать полный. 
-      # Джанго шаблоны ищет в папке "templates".
+  # Здесь уже нужно создать 
+  # первый шаблон. Путь 'store/index.html' здесь не нужно указывать полный. 
+  # Джанго шаблоны ищет в папке "templates".
+  return render(request, 'store/product_list.html', context={'products': products}) 
+
+
+def get_post_detail(request, product_id):
+  return render(request, 'store/product_detail.html', {"product": Products.objects.get(id=product_id)})
+  # return render(request, 'store/product_detail.html', {"product": get_object_or_404(Products, id=product_id)})
