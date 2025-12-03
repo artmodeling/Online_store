46029c50d68b1a3b486e4d2a855e1b18bd2274e4
Your Name <you@example.com>
2025-12-03 15:40:35 +0300

    feat(posts): Добавил функционал для добавления поста (самый простой вариант).

--- store/urls.py
+++ store/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     # path("", ) # Временно закомментировали маршрут на главную страницу. С ней будем работать позже.
     path("products/", views.get_product_list, name='product_list'),
-    path("products/<int:product_id>/", views.get_post_detail, name='product_detail'),
+    path("products/<int:product_id>/", views.get_product_detail, name='product_detail'),
+    path('products/add/', views.create_product),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from store.models import Products
 
@@ -13,6 +13,16 @@ def get_product_list(request):
   return render(request, 'store/product_list.html', context={'products': products}) 
 
 
-def get_post_detail(request, product_id):
-  return render(request, 'store/product_detail.html', {"product": Products.objects.get(id=product_id)})
-  # return render(request, 'store/product_detail.html', {"product": get_object_or_404(Products, id=product_id)})
+def get_product_detail(request, product_id):
+  # return render(request, 'store/product_detail.html', {"product": Products.objects.get(id=product_id)})
+  return render(request, 'store/product_detail.html', {"product": get_object_or_404(Products, id=product_id)})
+
+
+def create_product(request):
+  if request.method == "GET":
+    return render(request, 'store/product_add.html')
+  
+  if request.method == "POST":
+    product = Products.objects.create(product_name=request.POST.get('title'), about_item=request.POST.get('text'))
+
+    return redirect('product_detail', product_id=product.id)
