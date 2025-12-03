9a1c08296173261af5684988877c1b3cd02f2471
Your Name <you@example.com>
2025-12-03 15:41:42 +0300

    feat(nav): Добавил навигационное меню.

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # path("", ) # Временно закомментировали маршрут на главную страницу. С ней будем работать позже.
     path("products/", views.get_product_list, name='product_list'),
     path("products/<int:product_id>/", views.get_product_detail, name='product_detail'),
-    path('products/add/', views.create_product),
+    path('products/add/', views.create_product, name="new_product"),
 ]
